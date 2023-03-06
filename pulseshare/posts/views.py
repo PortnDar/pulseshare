3b6569e13b9dc45e1f24890b2a5e42d5002d5665
Портнова Дарья <ya.dashas1@yandex.ru>
2023-03-06 15:57:53 +0300

    Add app user Add app core

--- pulseshare/posts/views.py
+++ pulseshare/posts/views.py
@@ -1,14 +1,19 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post, Group
 
 
 def home(request):
+    posts = Post.objects.select_related('author', 'group').all()
     context = {
+        'posts': posts,
     }
     return render(request, 'posts/home.html', context)
 
 
 def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
     context = {
+        'group': group,
     }
     return render(request, 'posts/group_list.html', context)
